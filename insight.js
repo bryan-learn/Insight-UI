3db059af4822b9571b9d7a1aaf1a42eb040299c0
Bryan Learn <blearn@psc.edu>
2014-07-11 15:42:23 -0400

    undefined references fix

--- insight.js
+++ insight.js
@@ -84,19 +84,19 @@ this.Tuple = function (jso){
 }
 
 this.Flow = function (jso){
-	this.tuple = new Tuple(jso.tuple);
+	this.tuple = new ctrl.model.Tuple(jso.tuple);
 	this.priority = jso.priority;
 	this.dupAcks = jso.dupAcks;
 	this.oops = jso.oops;
 	this.cwnd = jso.cwnd;
 	this.winScale = jso.winScale;
-	this.latLng = new Location(jso.lat, jso.long);
+	this.latLng = new ctrl.model.Location(jso.lat, jso.long);
 	this.getID = function (){
 		// Call tuple's getID function
 		return this.tuple.getID();
 	};
 	this.drawn = false;
-}
+};
 
 
 /* Location
@@ -117,6 +117,7 @@ this.update = function (now){
 			this.sendMessage(1, '{"command":"exclude", "options":"9000"}');
 		}
 	}
+	requestAnimFrame(this.update);
 }.bind(this);
 
 /*
@@ -170,7 +171,7 @@ this.getRequest = function (url){
 this.getMessage = function (dataStr){
 	if(dataStr != null){
 		// Compare new data to old
-		this.processNewData(jsonToFlows(dataStr));
+		this.processNewData(this.jsonToFlows(dataStr));
 	}
 }.bind(this);
 
@@ -181,7 +182,7 @@ this.sendMessage = function (type, arg){
 		//return clientSend(); // temp function call to simulate client
 		break;
 	case 2:		// Send Report
-		report();
+		ctrl.view.report();
 		break;
 	}
 }.bind(this);
@@ -217,7 +218,7 @@ UIHandle = {
 function View() {
 
 // Define custom symbols
-this.nodeSym = {
+nodeSym = {
 	path: 'M0 0 m -8, 0 a 8,8 0 1,0 16,0 a 8,8, 0 1,0 -16,0',
 	fillColor: 'blue',
 	fillOpacity: 0.3,
@@ -226,13 +227,13 @@ this.nodeSym = {
 	scale: 0.5
 };
 
-this.lineSym = {
+lineSym = {
 	path: 'M 0,-1 0,1',
 	strokeOpacity: 1,
 	scale: 4
 };
 
-this.antSym = {
+antSym = {
 	path: 'm30.25,4.625l-6.375,2.875l-1.375,6.25l4.375,5l-3,4l-6.75,-2.5l-1,-4.625l-2.375,0.25l0.75,5.25l9,3.875l-0.5,4.75l-10.75,0l0.25,2.5l10.875,-0.125l1.625,4.125l-7.5,3.5l-3.5,5.625l2.125,1.125l3.5,-5.125l6.5,-2.125l-2.5,6.125l0.5,6.5l4.375,3.375l4.75,2.125l4.125,-2.125l3.25,-3.625l0.75,-6.625l-3.625,-5l7.875,1.5l3.375,3.25l2,-1.375l-3.25,-4.125l-8.25,-1.875l0.875,-4.875l10.625,-0.375l-0.125,-2.5l-10.625,0.375l-1.25,-6l8,-3.75l0.25,-5.125l-3,0.125l-0.25,3l-5.625,3.125l-1.75,-2.875l2.875,-4.25l-2,-7.5l-7.25,-2.125z',
 	fillColor: 'red',
 	fillOpacity: 1,
@@ -241,7 +242,7 @@ this.antSym = {
 	anchor: new google.maps.Point(30,30)
 };
 
-this.cloudSym = {
+cloudSym = {
 	path: 'm54.85827 61.440945l39.496063 -51.56693l136.04724 -9.874016l53.758514 29.622047l63.635193 -20.845144l105.32544 8.776903l57.05249 53.761158l103.131226 9.874016l17.553833 97.64566l-46.078735 65.82941l-26.333374 82.28346l-60.341187 53.76114l-86.67453 -28.524933l-63.635162 35.107635l-107.5197 4.3884277l-44.98163 -55.95276l-91.06299 1.0971375l-50.46982 -65.82941l7.6797905 -57.05249l-61.440945 -41.690292l3.2939632 -72.41207z',
 	fillColor: '#E3F8FA',
 	fillOpacity: 1,
@@ -255,7 +256,7 @@ this.pathClickEvent = function (e, contentStr){
 	/* Write Content to Connection Details Panel */
 	this.writeToInfoPanel(contentStr);
 	/* Pop-up Report Button */
-	var str = '<div style="width: 200px; height: 100px;"><p>Report this connection to the Network Operation Center?</p><button type="button" id="reportButton" onclick="report()">Yes</button></div>';
+	var str = '<div style="width: 200px; height: 100px;"><p>Report this connection to the Network Operation Center?</p><button type="button" id="reportButton" onclick="ctrl.view.report()">Yes</button></div>';
 	UIHandle.infoWindow.setContent(str);
 	UIHandle.infoWindow.setPosition(e.latLng);
 	UIHandle.infoWindow.open(UIHandle.map);
@@ -444,7 +445,7 @@ google.maps.event.addDomListener(window, 'load', this.view.mapInit);
 /* Update Loop Functions */
 
 // call Model's update loop
-
+requestAnimFrame(this.model.update);
 
 this.websockInit = function (){
 	window.WebSocket = window.WebSocket || window.MozWebSocket;
